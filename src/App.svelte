7542fe416e0bc1979a791a79d374abfb882c4989
Kibrands <jjextreme13@gmail.com>
2020-02-07 12:23:52 +0100

    Nav added

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,46 @@
 <script>
+  import { setContext } from "svelte";
+  import { Router } from "svelte-routing";
+  import Nav from "./Nav.svelte";
   import Footer from "./Footer.svelte";
   export let name;
 </script>
 
 <style>
+
 </style>
 
-<main>
+<header>
+  <Router>
+    <Nav />
+  </Router>
+</header>
+<main id="main">
   <h1>Hello {name}!</h1>
   <p>
     Visit the
     <a href="https://svelte.dev/tutorial">Svelte tutorial</a>
     to learn how to build Svelte apps.
   </p>
-  <Footer></Footer>
+  <Footer />
+  <!-- Font Awesome JS -->
+  <script src="vendors/fontawesome-free-5.11.2-web/js/all.js">
+
+  </script>
+  <!-- jQuery -->
+  <script src="vendors/bootstrap-4.4.0-dist/js/jquery-3.4.1.min.js">
+
+  </script>
+  <!-- Popper.js -->
+  <script src="vendors/bootstrap-4.4.0-dist/js/popper.min.js">
+
+  </script>
+  <!-- Bootstrap JS -->
+  <script src="vendors/bootstrap-4.4.0-dist/js/bootstrap.min.js">
+
+  </script>
+  <!-- Legal Advice -->
+  <script src="js/legalAdvice.js">
+
+  </script>
 </main>
---
+++ src/Nav.svelte
@@ -0,0 +1,56 @@
+<script>
+    import { Link } from "svelte-routing";
+    import App from './App.svelte';
+
+    let user = { loggedIn: false };
+
+	function toggle() {
+		user.loggedIn = !user.loggedIn;
+	}
+</script>
+<style></style>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark px-5">
+    <Link to="/">
+        <div class="navbar-brand">
+            <img class="float-left" src="img/favicon.png" width="60" alt="devpc icon">
+            <div class="float-left align-items-center ml-2">
+                <p>DevPC<br><span class="lower">Tu tienda de inform&aacute;tica</span></p>
+            </div>
+        </div>
+    </Link>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse"
+        aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <Link to="/">
+                    <div class="nav-item nav-link">
+                        <i class="fas fa-home"></i>&nbsp;Inicio
+                    </div>
+                </Link>
+            </li>
+            <li class="nav-item">
+                <Link to="/contact">
+                    <div class="nav-item nav-link">
+                        <i class="fas fa-headset"></i>&nbsp;Contacto
+                    </div>
+                </Link>
+            </li>
+        </ul>
+        <div class="form-inline mt-2 mt-md-0">
+            <button class="btn btn-outline-light my-2 ml-2 my-sm-0" id="logIn"
+                 on:click={toggle}>
+            {#if user.loggedIn}
+                Log out
+            {/if}
+
+            {#if !user.loggedIn}
+                Log in
+            {/if}
+            </button>
+        </div>
+    </div>
+</nav>
