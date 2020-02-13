9b565c8df970cdecdf594f2341a94eeb9c2c1bbc
Kibrands <jjextreme13@gmail.com>
2020-02-13 09:06:58 +0100

    register page added

---
+++ src/Register.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { Link } from "svelte-routing";
+  import { md5 } from './md5.js';
+</script>
+
+<nav aria-label="breadcrumb">
+  <ol class="breadcrumb">
+    <li class="breadcrumb-item"><Link to="/"><i class="fas fa-home"></i> Inicio</Link></li>
+    <li class="breadcrumb-item active" aria-current="page">Registro</li>
+  </ol>
+</nav>
+<div class="container">
+    <h2>Registro</h2>
+    <form>
+
+    </form>
+</div>
