798f8aa01cc784094bc69941e3ff0b3719a20e8b
Álvaro del Moral <43779971+alvarodelmoral@users.noreply.github.com>
2020-02-19 10:59:51 +0100

    Add files via upload

--- public/js/legalAdvice.js
+++ public/js/legalAdvice.js
@@ -1,29 +1,36 @@
 $(document).ready(function () {
     var aviso = `
-    El Ayuntamiento de Écija, a través del dominio ecija.es facilita información de interés general de la ciudad de Écija y, en especial, información y gestiones propias de la organización municipal.
-    <br>
+    En cumplimiento del artículo 10 de la Ley 34 / 2002, de 11 de julio, de Servicios de la Sociedad de la Información y Comercio Electrónico, el Titular expone sus datos identificativos.    <br>
     La información facilitada es la vigente en el momento de su publicación.
+    <br>  <br>
+      La finalidad del sitio Web URL es ofrecer los distintos productos más actuales de componentes informáticos que se muestran a partir del sitio web.
+    <br>    <br>
+    La utilización del sitio Web le otorga la condición de Usuario, e implica la aceptación completa de todas las cláusulas y condiciones de uso incluidas en las páginas:
+    <br>    <br>
+    • Aviso Legal    <br>
+    • Política de Privacidad    <br>
+    • Política de Cookies    <br>
     <br>
-    El Ayuntamiento de Écija procura que esta información sea exacta y precisa, y procede a su actualización con la máxima celeridad posible, tratando de evitar errores y corrigiéndolos tan pronto como los detecta. No obstante, el Ayuntamiento de Écija no puede garantizar la inexistencia de errores ni que el contenido de la información se encuentre permanentemente actualizado.
-    <br>
-    El Ayuntamiento de Écija podrá efectuar, en cualquier momento y sin necesidad de previo aviso, modificaciones, supresiones o actualizaciones de la información contenida en ecija.es o en su configuración o presentación.
-    <br>
-    El acceso al dominio ecija.es, así como el uso de la información que contiene, son de la exclusiva responsabilidad del usuario. El Ayuntamiento de Écija no es responsable de ningún perjuicio que pudiera derivarse de esos hechos, ni tampoco puede garantizar que el acceso no se vea interrumpido o que el contenido o software al que pueda accederse esté libre de error o de causar daño.
-    <br>
-    La información proporcionada en respuesta a cualquier consulta o petición de información tiene carácter meramente orientativo y en ningún caso resultará vinculante para la resolución de los procedimientos administrativos, los cuales se hallan estrictamente sujetos a las normas legales o reglamentarias que les sean de aplicación.
-    <br>
-    En ecija.es se incluyen enlaces a páginas de sitios web de terceros, fundamentalmente de otras Administraciones públicas, empresas locales y asociaciones de la ciudad, que se considera pueden ser de interés para los usuarios. El Ayuntamiento de Écija no asume ninguna responsabilidad derivada de las conexiones o los contenidos de esos enlaces.
-    <br>
-    El portal ecija.es, sus diseños gráficos y las informaciones y códigos que contiene son de titularidad del Ayuntamiento de Écija, salvo expresa indicación de una titularidad diferente, y están protegidos por la legislación sobre propiedad intelectual.
-    <br>
-    Los contenidos de este dominio pueden ser descargados en el terminal del usuario siempre que sea para su uso privado y sin ningún fin comercial, pero no pueden ser objeto de explotación, reproducción, distribución, modificación, comunicación pública, cesión o transformación, salvo autorización específica y expresa.
+
+
+    Si no estuviera conforme con todas y cada una de estas cláusulas y condiciones absténgase de utilizar este sitio Web.
+    <br><br>
+
+    El acceso a este sitio Web no supone, en modo alguno, el inicio de una relación comercial con el Titular.
+    <br><br>
+
+    A través de este sitio Web, el Titular le facilita el acceso y la utilización de diversos contenidos que el Titular o sus colaboradores han publicadon por medio de Internet.
+    <br><br>
+
+    A tal efecto, usted está obligado y comprometido a NO utilizar cualquiera de los contenidos del sitio Web con fines o efectos ilícitos, prohibidos en este Aviso Legal o por la legislación vigente, lesivos de los derechos e intereses de terceros, o que de cualquier forma puedan dañar, inutilizar, sobrecargar, deteriorar o impedir la normal utilización de los contenidos, los equipos informáticos o los documentos, archivos y toda clase de contenidos almacenados en cualquier equipo informático propios o contratados por el Titular, de otros usuarios o de cualquier usuario de Internet.
     `;
     var contact = `
     Teléfono: 954 024 209<br>
-    c/. San Francisco, 11 - Planta Baja<br>
-    Ayuntamiento de Écija<br>
+    c/. San Juan, 11 - Planta Baja<br>
+    DivPc<br>
     `;
     $('#legalAdviceContact').html(contact);
     $('#legalAdviceContent').html(aviso);
 });
+
 
