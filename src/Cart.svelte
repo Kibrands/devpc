af92c4de790f552c42607636279c7be705d19970
Kibrands <jjextreme13@gmail.com>
2020-02-26 10:13:35 +0100

    functional cart

--- src/Cart.svelte
+++ src/Cart.svelte
@@ -9,52 +9,75 @@
   let cart = {};
   let search = "";
   let category = "";
-  let product = {};
-  let amount = [];
-  let amountSelected = 1;
+  let product = [{ title: "a", image: "a" }];
+
+  function isIncluded(carts, data) {
+    let inBool = false;
+    for (let i = 0; i < carts.length; i++) {
+      if (carts[i].productId == data.productId) inBool = true;
+    }
+    return inBool;
+  }
+
+  function prepare(data) {
+    for (let i = 0; i < data.length; i++) {
+      if (isIncluded($carts, data[i])) {
+        $carts.forEach(element => {
+          if (element.productId == data[i].productId) {
+            element.amount++;
+          }
+        });
+      } else {
+        if ($carts.length <= 0) {
+          $carts[0] = data[i];
+        } else {
+          $carts[$carts.length] = data[i];
+        }
+      }
+    }
+  }
 
   onMount(async () => {
     const response = await fetch(URL.carts + "user/" + user.data._id);
     const data = await response.json();
-    $carts = await data;
+    await prepare(data);
   });
 
-  async function getProductTitle(cart) {
+  async function getProductTitle(cart, i) {
     const resp = await fetch(URL.products + cart.productId);
     const data = await resp.json();
-    product = await data;
-    for (let i = 1; i <= (await product.stock); i++) {
-      amount[i - 1] = i;
-    }
+    product[i] = await data;
+    return product[i];
   }
 </script>
 
 <div class="container">
   <br />
   <div class="row">
-    {#each $carts as cart}
-      <input type="hidden" value={getProductTitle(cart)} />
-      <div class="card col-12">
-        <div class="row">
-          <div class="col-2">
-            <img
-              class="img-fluid border rounded my-2"
-              width="200"
-              src={product.image}
-              alt={product.title} />
-          </div>
-          <div class="col-10">
-            <h5>{product.title}</h5>
-            <label for="amount">Cantidad</label>
-            <select name="amount" id="amount" bind:value={amountSelected}>
-              {#each amount as option}
-                <option value={option}>{option}</option>
-              {/each}
-            </select>
-            <Button type="deleteCart" document={cart} collection="carts" />
+    {#each $carts as cart, i}
+      {#await getProductTitle(cart, i) then prod}
+        <div class="card col-12">
+          <div class="row">
+            <div class="col-2">
+              <img
+                class="img-fluid border rounded my-2"
+                width="200"
+                src={prod.image}
+                alt={prod.title} />
+            </div>
+            <div class="col-10">
+              <h5>{prod.title}</h5>
+              <label for="amountSelected">Cantidad seleccionada</label>
+              <input type="number" value={cart.amount} />
+              <p>Precio x 1: <b>{prod.price} &euro;</b></p>
+              {#if cart.amount > 1}
+                <p>Precio x {cart.amount}: <b>{prod.price * cart.amount} &euro;</b></p>
+              {/if}
+              <Button type="deleteCart" document={cart} collection="carts" />
+            </div>
           </div>
         </div>
-      </div>
+      {/await}
     {/each}
   </div>
 </div>
