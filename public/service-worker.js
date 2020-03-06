f047e054e175abd08392d0099be253fa22d907ce
Kibrands <jjextreme13@gmail.com>
2020-03-06 13:57:08 +0100

    new fetch cache function

--- public/service-worker.js
+++ public/service-worker.js
@@ -65,18 +65,29 @@ self.addEventListener("activate", function(e) {
 
 // FETCH
 // Hacemos peticiones a recursos.
-self.addEventListener('fetch', function(e) {
-    console.log("[Service Worker] * Fetch.");
+self.addEventListener('fetch', (event) => {
+    event.respondWith((async() => {
+        try {
+            // First, try to use the navigation preload response if it's supported.
+            const preloadResponse = await event.preloadResponse;
+            if (preloadResponse) {
+                return preloadResponse;
+            }
 
-    // Hacemos petición a la red y si no está disponible obtenemos desde la caché
-    e.respondWith(fetch(e.request)
-        .catch(function() {
-            return caches.open(CACHE_NAME)
-                .then((cache) => {
-                    return cache.match('/offline.html');
-                });
-        }));
+            const networkResponse = await fetch(event.request);
+            return networkResponse;
+        } catch (error) {
+            // catch is only triggered if an exception is thrown, which is likely
+            // due to a network error.
+            // If fetch() returns a valid HTTP response with a response code in
+            // the 4xx or 5xx range, the catch() will NOT be called.
+            console.log('Fetch failed; returning offline page instead.', error);
 
+            const cache = await caches.open(CACHE_NAME);
+            const cachedResponse = await cache.match("/offline.html");
+            return cachedResponse;
+        }
+    })());
 });
 
 
